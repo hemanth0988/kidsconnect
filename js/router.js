9bbc28d1bb064168199940430fa8c3f0212344d2
hemanth0988 <hemanth0988@gmail.com>
2025-12-29 08:51:50 +0400

    Add forgot password route to router

--- js/router.js
+++ js/router.js
@@ -1,9 +1,11 @@
 import { renderLogin } from "./screens/login.js";
 import { renderSignup } from "./screens/signup.js";
+import { renderForgot } from "./screens/forgot.js";
 
 const routes = {
   login: renderLogin,
   signup: renderSignup,
+  forgot: renderForgot,
 };
 
 export function goTo(routeName) {
